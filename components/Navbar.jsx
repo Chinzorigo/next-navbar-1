0fbc4fcda301aea95a4299913194b41f01cfb2e4
Chinzorigo <chinzorig.o@gmail.com>
2023-03-29 00:47:50 +0800

    mobile menu open has been added

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,10 +1,15 @@
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../public/logo.png";
-import { AiOutlineMenu } from 'react-icons/ai'
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleNav = () => {
+    setMenuOpen(!menuOpen);
+  };
   return (
     <nav className="fixed w-full h-24 shadow-xl bg-white">
       <div className="flex justify-between items-center h-full w-full px-4 2xl:px-16">
@@ -18,25 +23,39 @@ function Navbar() {
             priority
           />
         </Link>
-        <div>
+        <div className="hidden sm:flex">
           <ul className="hidden sm:flex">
             <Link href="/about">
               <li className="ml-10 uppercase hover:border-b text-xl">Why Us</li>
             </Link>
             <Link href="/services">
-              <li className="ml-10 uppercase hover:border-b text-xl">Our Services</li>
+              <li className="ml-10 uppercase hover:border-b text-xl">
+                Our Services
+              </li>
             </Link>
             <Link href="/blog">
               <li className="ml-10 uppercase hover:border-b text-xl">Blog</li>
             </Link>
             <Link href="/contact">
-              <li className="ml-10 uppercase hover:border-b text-xl">Contact Us</li>
+              <li className="ml-10 uppercase hover:border-b text-xl">
+                Contact Us
+              </li>
             </Link>
           </ul>
         </div>
-        <div className="md:hidden cursor-pointer text-xl">
-            <AiOutlineMenu size={25} />
+        <div onClick={handleNav} className="sm:hidden cursor-pointer text-xl">
+          <AiOutlineMenu size={25} />
         </div>
+      </div>
+      <div
+        className={
+          menuOpen
+            ? "fixed left-0 top-0 w-[65%] sm:hidden h-screen bg-[#ecf0f3] p-10 ease-in duration-500"
+            : "fixed left-[-100%] top-0 p-10 ease-in duration-500"
+        }>
+            <div className="flex w-full items-center justify-end">
+                <AiOutlineClose size={25} />
+            </div>
       </div>
     </nav>
   );
