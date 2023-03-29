a924e7246171e672371a50109d84d249a6e8dbf9
Chinzorigo <chinzorig.o@gmail.com>
2023-03-29 22:44:02 +0800

    responsive navbar has done.

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,7 +2,13 @@ import Image from "next/image";
 import Link from "next/link";
 import React, { useState } from "react";
 import Logo from "../public/logo.png";
-import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+import {
+  AiOutlineMenu,
+  AiOutlineClose,
+  AiOutlineInstagram,
+  AiOutlineFacebook,
+  AiOutlineTwitter,
+} from "react-icons/ai";
 
 function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -25,10 +31,10 @@ function Navbar() {
         </Link>
         <div className="hidden sm:flex">
           <ul className="hidden sm:flex">
-            <Link href="/about">
-              <li className="ml-10 uppercase hover:border-b text-xl">Why Us</li>
+            <Link href="/">
+              <li className="ml-10 uppercase hover:border-b text-xl">Home</li>
             </Link>
-            <Link href="/services">
+            <Link href="/service">
               <li className="ml-10 uppercase hover:border-b text-xl">
                 Our Services
               </li>
@@ -52,10 +58,66 @@ function Navbar() {
           menuOpen
             ? "fixed left-0 top-0 w-[65%] sm:hidden h-screen bg-[#ecf0f3] p-10 ease-in duration-500"
             : "fixed left-[-100%] top-0 p-10 ease-in duration-500"
-        }>
-            <div className="flex w-full items-center justify-end">
-                <AiOutlineClose size={25} />
-            </div>
+        }
+      >
+        <div className="flex w-full items-center justify-end">
+          <div onClick={handleNav} className="cursor-pointer">
+            <AiOutlineClose size={25} />
+          </div>
+        </div>
+        <div className="flex-col py-4">
+          <ul>
+            <Link href="/">
+              <li
+                onClick={() => setMenuOpen(false)}
+                className="py-4 cursor-pointer"
+              >
+                Home
+              </li>
+            </Link>
+            <Link href="/service">
+              <li
+                onClick={() => setMenuOpen(false)}
+                className="py-4 cursor-pointer"
+              >
+                Our Services
+              </li>
+            </Link>
+            <Link href="/blog">
+              <li
+                onClick={() => setMenuOpen(false)}
+                className="py-4 cursor-pointer"
+              >
+                Blog
+              </li>
+            </Link>
+            <Link href="/contact">
+              <li
+                onClick={() => setMenuOpen(false)}
+                className="py-4 cursor-pointer"
+              >
+                Contact Us
+              </li>
+            </Link>
+          </ul>
+        </div>
+        <div className="flex flex-row justify-around pt-10 items-center">
+          <AiOutlineInstagram size={30} className="cursor-pointer" />
+          <AiOutlineFacebook size={30} className="cursor-pointer" />
+          <AiOutlineTwitter size={30} className="cursor-pointer" />
+        </div>
+        <div className="flex justify-center items-center py-6">
+        <Link href="/">
+          <Image
+            src={Logo}
+            alt="Logo"
+            width="65"
+            height="65"
+            className="cursor-pointer "
+            priority
+          />
+        </Link>
+        </div>
       </div>
     </nav>
   );
